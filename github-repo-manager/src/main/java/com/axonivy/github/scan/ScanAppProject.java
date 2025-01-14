f65b9c70e6b6f891afa5c8dcd6330b57919f82ff
nqhoan-axonivy <hoan.nguyen01@axonactive.com>
2025-01-14 10:38:35 +0700

    Fix wrong input

--- github-repo-manager/src/main/java/com/axonivy/github/scan/ScanAppProject.java
+++ github-repo-manager/src/main/java/com/axonivy/github/scan/ScanAppProject.java
@@ -3,11 +3,17 @@ package com.axonivy.github.scan;
 import com.axonivy.github.Logger;
 import com.axonivy.github.scan.util.ScanUtils;
 import com.axonivy.github.util.GitHubUtils;
+import org.apache.commons.lang3.StringUtils;
 
 public class ScanAppProject {
   private static final Logger LOG = new Logger();
 
   public static void main(String[] args) {
+    String repo = ScanUtils.getProceedRepo();
+    if (StringUtils.isBlank(repo)) {
+      LOG.info("No repo was submitted, job ended!");
+      System.exit(0);
+    }
     try {
       var marketAppProjectScanner = new MarketAppProjectScanner(GitHubUtils.extractActor(args), ScanUtils.getProceedRepo());
       marketAppProjectScanner.proceed();
