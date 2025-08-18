9dc73d51cd99b416ec66939b7e733aca5b2d8817
ttan-axonivy <an.truongthien@axonactive.com>
2025-08-18 16:25:55 +0700

    update json

--- github-repo-manager/src/main/java/com/axonivy/github/file/GitHubMissingFilesDetector.java
+++ github-repo-manager/src/main/java/com/axonivy/github/file/GitHubMissingFilesDetector.java
@@ -51,11 +51,8 @@ public class GitHubMissingFilesDetector {
     if (GITHUB_ORG.equals(repo.getName())) {
       return;
     }
-    if (repo.isFork()) {
-      return;
-    }
-    if (repo.isPrivate() || repo.isArchived()) {
-      LOG.info("Repo {0} is {1}.", repo.getFullName(), repo.isPrivate() ? "private" : "archived");
+    if (repo.isPrivate()) {
+      LOG.info("Repo {0} is private.", repo.getFullName());
       return;
     }
 
